8f21c7982997cd84c30481a975a521bc469e0e7c
waseem anwar <waseemanwargiccl@gmail.com>
2022-06-14 22:57:13 -0400

    Added ssh option in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,11 +6,11 @@ set :repo_url, "git@github.com:waseemanwar1/brandlisten.git"
 set :branch, :main
 set :deploy_to, '/home/deploy/brandlisten'
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.2.3' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, 'ruby-3.0.3' # Edit this if you are using MRI Ruby
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
@@ -19,6 +19,7 @@ set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array f
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :ssh_options, forward_agent: true, user: fetch(:user), keys: %w[~/.ssh/brandlisten.pem]
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 8]
